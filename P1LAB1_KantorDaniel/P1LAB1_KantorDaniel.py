3fd6941e8d4c88b2b81dec7a83ca9d485077c166
DKantor2002 <d.kantor.dev@gmail.com>
2026-01-28 10:51:12 -0500

    Completed P1LAB1

---
+++ P1LAB1_KantorDaniel/P1LAB1_KantorDaniel.py
@@ -0,0 +1,10 @@
+# Daniel Kantor
+# January 28, 2026
+# P1LAB1
+# This project takes a user's input and returns a message including that user's input.
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello, " + first_name + " " + last_name + "! Welcome to CTI-110")
