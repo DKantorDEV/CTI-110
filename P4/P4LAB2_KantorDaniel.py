4a974d7d6d3aa97fe0d8c62559ec04f52dd20d14
DKantorDEV <d.kantor.dev@gmail.com>
2026-02-14 08:59:34 -0500

    Fixed some loop errors

--- P4/P4LAB2_KantorDaniel.py
+++ P4/P4LAB2_KantorDaniel.py
@@ -7,23 +7,17 @@
 run_program = True
 
 while run_program:
-
     user_number = int(input("\nEnter an integer: "))
 
-    print('\n')
-
     if user_number < 0:
-        print("This program does not handle negative numbers.\n")
-        repeat_program = input("Would you like to run the program again? ")
-        if repeat_program != "yes":
-            run_program = False
-            print("\nExiting program...")
-    else:
-        for _ in range(1, 13):
-            result = user_number * _
-            print(f"{user_number} * {_} = {result}")
+        print("\nThis program does not handle negative numbers.")
 
-    repeat_program = input("\nWould you like to run the program again? ")
+    else:
+        for i in range(1, 13):
+            result = user_number * i
+            print(f"{user_number} * {i} = {result}")
+
+    repeat_program = input("\nWould you like to run the program again? (yes/no): ").lower()
     if repeat_program != "yes":
-            run_program = False
-            print("\nExiting program...\n")
+        run_program = False
+        print("\nExiting program...\n")
